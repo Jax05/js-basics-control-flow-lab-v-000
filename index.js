cea8192dc55739ff37899499dbd1a60d7d302762
Carley Tripp <carley.tripp@outlook.com>
2018-10-27 03:01:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
   let message;
-  if (feet <= 400) {
-    message = "This one is on me!"
+  if (feet > 2500) {
+    message = "No can do."
   } else if (feet > 2000) {
     message = "I will gladly take your thirty bucks."
-  } else if (feet > 2500) {
-    message = "No can do."
+  } else if (feet <= 400) {
+    message = "This one is on me!"
   }
   return message
 }
